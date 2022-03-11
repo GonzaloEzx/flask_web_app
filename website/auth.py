dd1dd2b2ca21757ecaa67358844b41372d0a95e6
Gonzalo Martin Estevez <gonzalomartinestevez@gmail.com>
2022-03-11 19:28:31 -0300

    APPtest

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,9 @@
-
 from flask import Blueprint, render_template
 
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
 	return render_template("login.html", boolean=False)
 
@@ -12,7 +11,7 @@ def login():
 def logout():
 	return "<h1>Logout</h1>"	
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
 	return render_template("sign_up.html")
 
